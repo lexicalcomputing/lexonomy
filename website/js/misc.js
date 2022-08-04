d949bf65d787c6d38292f54d1c7b25c8a09cdeea
Tomáš Svoboda <tomas.svoboda@sketchengine.co.uk>
2022-08-04 16:24:09 +0200

    Added show tooltip function

--- website/js/misc.js
+++ website/js/misc.js
@@ -1,3 +1,31 @@
+window.showTooltip = (selector, message, delay, options) => {
+   let node = $(selector)
+   let instance = M.Tooltip.getInstance(node)
+   if(instance){
+      instance.destroy()
+   }
+
+
+   node.tooltip(Object.assign({
+      enterDelay: delay || 0,
+      exitDelay: 500,
+      html: message
+   }, options || {}))
+   let tooltip = M.Tooltip.getInstance(node)
+   setTimeout(function(node){
+      if($(node).is(":hover")){
+         let tooltip = M.Tooltip.getInstance(node)
+         tooltip && tooltip.open()
+      }
+   }.bind(null, node), delay || 0)
+   node.one("mouseleave", function(){
+      !this.isOpen && this.destroy()
+   }.bind(tooltip))
+
+   return tooltip
+}
+
+
 window.editorNeedsSaving = (evt) => {
    if(Screenful && Screenful.Editor && Screenful.Editor.needsSaving){
       if(!confirm(Screenful.Loc.unsavedConfirm)){
