d1071a87276cafd8d1ddefe0ab366851e2752d37
Marek Medveď <marek.medved@sketchengine.eu>
2024-01-25 13:52:07 +0100

    update for SQLite 3.45

--- website/json_search.py
+++ website/json_search.py
@@ -1,104 +1,119 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # coding: utf-8
 # Author: Marek Medved, marek.medved@sketchengine.eu, Lexical Computing CZ
 import os
 import re
 import sys
-import sqlite3
+import pysqlite3 as sqlite3
 import unittest
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 def regexp(expr, item):
     reg = re.compile(expr)
     return reg.search(item) is not None
 
-def query2sqliteToken(token):
+
+def query2sqliteToken(token, all_json_trees):
     match token:
         case _ if '!=' in token:
             # !=
             parts = token.split('!=')
-            operator = '!='
             path = parts[0]
             value = parts[1]
+            operator = '!='
         case _ if '~=' in token:
             #regex
             parts = token.split('~=')
-            operator = '~='
             path = parts[0]
             value = parts[1]
+            operator = '~='
         case _ if '#=' in token:
             #count
             parts = token.split('#=')
-            operator = '#='
             path = parts[0]
             value = parts[1]
+            if value == '0':
+                operator = 'not_exist'
+            else:
+                operator = '#='
         case _ if '#>' in token:
             #count
             parts = token.split('#>')
-            operator = '#>'
             path = parts[0]
             value = parts[1]
+            operator = '#>'
         case _ if '#<' in token:
             #count
             parts = token.split('#<')
-            operator = '#<'
             path = parts[0]
             value = parts[1]
+            if value == '1':
+                operator = 'not_exist'
+            else:
+                operator = '#<'
         case _ if '=' in token:
             # =
             parts = token.split('=')
-            operator = '='
             path = parts[0]
             value = parts[1]
+            operator = '='
         case _:
-            operator = 'exist'
             path = token.strip()
             value = ''
+            operator = 'exist'
     
-    # fullpath = path.replace('.', '[%].')
-    # fullpathval = '$.' + fullpath + '[%]._value'
+    if '_' in path:
+        fullpath = '$.%.' + '"' + path + '"'
+    else:
+        fullpath = '$.%.' + path
 
-    fullpath = '$.%.' + path + '[%]'
-    fullpathval = fullpath + '._value'
+    fullpathval = fullpath + '[%]' + '."_value"'
 
     sql = ''
+
+    # for and/or queries
+    json_tree = 'json_tree'
+    if all_json_trees:
+        json_tree = all_json_trees.pop(0)
+
     match operator:
         case '=':
-            sql = "(json_tree.key='_value' AND json_tree.value = '" + value + "' AND json_tree.fullkey LIKE '" + fullpathval + "')"
+            sql = "(" + json_tree + ".key='_value' AND " + json_tree + ".value = '" + value + "' AND " + json_tree + ".fullkey LIKE '" + fullpathval + "')"
         case '!=':
-            sql = "(json_tree.key='_value' AND json_tree.value != '" + value + "' AND json_tree.fullkey LIKE '" + fullpathval + "')"
+            sql = "(" + json_tree + ".key='_value' AND " + json_tree + ".value != '" + value + "' AND " + json_tree + ".fullkey LIKE '" + fullpathval + "')"
         case 'exist':
-            sql = "(json_tree.fullkey LIKE '" + fullpath + "')"
+            sql = "(" + json_tree + ".fullkey LIKE '" + fullpath + '[%]' + "')"
+        case 'not_exist':
+            sql = "entries.id NOT IN (SELECT DISTINCT entries.id from entries, " + json_tree + "(entries.json) where (" + json_tree + ".fullkey LIKE '" + fullpathval + "'))"
         case '~=':
-            sql = "(json_tree.key='_value' AND json_tree.value REGEXP '" + value + "' AND json_tree.fullkey LIKE '" + fullpathval + "')"
+            sql = "(" + json_tree + ".key='_value' AND " + json_tree + ".value REGEXP '" + value + "' AND " + json_tree + ".fullkey LIKE '" + fullpathval + "')"
         case '#=':
-            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.fullkey LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)=" + value + "))"
+            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.path LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)=" + value + "))"
         case '#>':
-            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.fullkey LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)>" + value + "))"
+            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.path LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)>" + value + "))"
         case '#<':
-            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.fullkey LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)<" + value + "))"
+            sql = "(entries.id IN (SELECT entries.id FROM entries,json_tree(entries.json) WHERE json_tree.path LIKE '" + fullpath + "' GROUP BY entries.id HAVING COUNT(json_tree.key)<" + value + "))"
     return sql
 
 
-def query2sqliteQuery(query_list):
+def query2sqliteQuery(query_list, all_json_trees):
     result_list = []
     for token in query_list:
         if type(token) == list:
-            result_list.append(query2sqliteQuery(token))
+            result_list.append(query2sqliteQuery(token, all_json_trees))
         else:
             if token == 'and' or token == 'or':
                 result_list.append(token)
             else:
-                result_list.append(query2sqliteToken(token))
+                result_list.append(query2sqliteToken(token, all_json_trees))
     return '(' + ' '.join(result_list) + ')'
 
 
-def parse_nested(text, left=r'[(]', right=r'[)]', sep=r' '):
+def get_query_tokens(text, left=r'[(]', right=r'[)]', sep=r' '):
     pat = r'({}|{}|{})'.format(left, right, sep)
     
     tokens = re.split(pat, text)
-    # print(tokens)
-    # sys.exit()
     stack = [[]]
     for x in tokens:
         if not x or re.match(sep, x): continue
@@ -112,17 +127,33 @@ def parse_nested(text, left=r'[(]', right=r'[)]', sep=r' '):
         else:
             stack[-1].append(x)
     if len(stack) > 1:
-        print(stack)
         raise ValueError('error: closing bracket is missing')
     return stack.pop()
 
 
 def dql2sqlite(query):
-    parsed_query = parse_nested(query)
-    # distinct - different
-    # json_tree - table-valued function that walks the JSON value provided as its first argument and returns a table consisting of one row for each array element or object member
-    # TODO change
-    sql = "select distinct entries.id, entries.json from entries, json_tree(entries.json) where " + query2sqliteQuery(parsed_query) + " limit 10;"
+    def make_json_trees(query_tokens, sql, tree_id, all_json_trees):
+        for item in query_tokens:
+            if isinstance(item, list):
+                tree_id = make_json_trees(item, sql, tree_id, all_json_trees)
+            elif isinstance(item, str) and item not in ['and', 'or']:
+                sql.append(f',json_tree(entries.json) AS tree{tree_id} ')
+                all_json_trees.append(f'tree{tree_id}')
+                tree_id += 1
+        return tree_id
+
+    query_tokens = get_query_tokens(query)
+    all_json_trees = []
+    if 'and' in query_tokens or 'or' in query_tokens:
+        # Need to perform each condition on own json_tree
+        # select distinct entries.id, entries.json from entries, json_tree(entries.json) AS sense,  json_tree(entries.json) AS flag 
+        # where ((sense.fullkey LIKE '$.%.sense[%]') and 
+        # (flag.key='_value' AND flag.value = 'nok' AND flag.fullkey LIKE '$.%.flag[%]._value')) limit 10;
+        sql = ["SELECT DISTINCT entries.id, entries.json FROM entries"]
+        make_json_trees(query_tokens, sql, 0, all_json_trees)
+        sql = ''.join(sql) + " where " + query2sqliteQuery(query_tokens, all_json_trees) + " limit 10;"
+    else:
+        sql = "SELECT DISTINCT entries.id, entries.json FROM entries, json_tree(entries.json) WHERE " + query2sqliteQuery(query_tokens, all_json_trees) + " limit 10;"
     return sql
 
 
@@ -144,17 +175,16 @@ class TestQueries(unittest.TestCase):
     def setUp(self):
         self.db = sqlite3.connect(f'{current_dir}/tests/test.sqlite')
 
-    def test_key_exists(self):
+    def test_key_exists(self): # OK
         query = dql2sqlite('sense')
-        print(query)
         self.assertListEqual(result_id_list(query, self.db), [1, 2, 3, 5])
 
     # VALUES
-    def test_value_equals(self): # OK
+    def test_value_equals(self): # OK
         query = dql2sqlite('sense=test_5')
         self.assertListEqual(result_id_list(query, self.db), [5])
 
-    def test_value_re_equals(self):
+    def test_value_re_equals(self): # OK
         query = dql2sqlite('sense~=test_2.*')
         self.assertListEqual(result_id_list(query, self.db), [2])
     
@@ -164,30 +194,20 @@ class TestQueries(unittest.TestCase):
         self.assertListEqual(result_id_list(query, self.db), [1, 2, 3])
 
     def test_count_less_than(self):
-        query = dql2sqlite('image#<1')
-        print(query)
-        self.assertListEqual(result_id_list(query, self.db), [1, 2, 3, 4])
+        query = dql2sqlite('image#<2')
+        self.assertListEqual(result_id_list(query, self.db), [3])
     
     def test_count_equals(self):
         query = dql2sqlite('sense#=0')
         self.assertListEqual(result_id_list(query, self.db), [4])
 
-    # def test_count_condition(self):
+    # def test_count_condition(self): # TODO
     #     query = dql2sqlite('example#>0.quality=bad')
     #     self.assertListEqual(result_id_list(query, self.db), [4])
 
     # OPERATORS
     def test_and_operator(self):
-        # OLD
-        # select distinct entries.id, entries.json from entries, json_tree(entries.json) 
-        # where ((json_tree.fullkey LIKE '$.%.sense[%]') and 
-        # (json_tree.key='_value' AND json_tree.value = 'nok' AND json_tree.fullkey LIKE '$.%.flag[%]._value')) limit 10;
-        # NEW
-        # select distinct entries.id, entries.json from entries, json_tree(entries.json) AS sense,  json_tree(entries.json) AS flag 
-        # where ((sense.fullkey LIKE '$.%.sense[%]') and 
-        # (flag.key='_value' AND flag.value = 'nok' AND flag.fullkey LIKE '$.%.flag[%]._value')) limit 10;
         query = dql2sqlite('sense and flag=nok')
-        print(query)
         self.assertListEqual(result_id_list(query, self.db), [3])
 
     def test_or_operator(self):
@@ -196,5 +216,5 @@ class TestQueries(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    print('SQLite version: ' + sqlite3.connect(':memory:').execute('SELECT sqlite_version();').fetchall()[0][0])
     unittest.main()
-    # main()
