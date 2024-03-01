4d52af6c1e80c36fa86a49b1d5a5137c76414c71
Marek Medveď <marek.medved@sketchengine.eu>
2024-03-01 10:58:47 +0100

    advance search: new tests + renaming

--- website/tests/advance_search_test.py
+++ website/tests/advance_search_test.py
@@ -9,6 +9,7 @@ import unittest
 sys.path.append('../')
 from advance_search import result_id_list
 from advance_search import get_query_parts
+from advance_search import get_sql_query
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -48,16 +49,20 @@ class TestQueries(unittest.TestCase):
     def test_and_operator(self):
         self.assertListEqual(result_id_list('sense and flag="nok"', self.db), [3])
 
-    def test_incluse_and_operator(self):
+    def test_where_operator(self):
         self.assertListEqual(result_id_list('s_image#="1" where s_i_quality="bad"', self.db), [2])
 
-    def test_incluse_and_operatori_2(self):
+    def test_where_operator_2(self):
         self.assertListEqual(result_id_list('i_quality="good" where i_license="general"', self.db), [3, 5])
 
-    def test_incluse_and_operatori_3(self):
+    def test_where_operator_3(self):
         self.assertListEqual(result_id_list('i_quality="good" where i_license="general" where i_author="LCC"', self.db), [3, 5])
+    
+    def test_where_operator_4(self):
+        # TODO the restriction on tree.fullpath is not enough need to somehow apply the count again
+        self.assertListEqual(result_id_list('sense#="2" where (s_e_quality="good")', self.db), [1])
 
-    def test_incluse_and_operator_with_or(self):
+    def test_where_operator_with_or(self):
         self.assertListEqual(result_id_list('s_image#="1" where (s_i_quality="bad" or s_i_quality="low")', self.db), [2])
 
     def test_or_operator(self):
@@ -74,6 +79,12 @@ class TestQueries(unittest.TestCase):
     def test_query_parse_3(self):
         query = ' ( definition != "asda*"   and   (headword != "sads"   and  (   example_translation != "akk"  )  )    )  '
         self.assertEqual(get_query_parts(query), [[{'attr': 'definition', 'op': '!=', 'val': 'asda*'}, 'and', [{'attr': 'headword', 'op': '!=', 'val': 'sads'}, 'and', [{'attr': 'example_translation', 'op': '!=', 'val': 'akk'}]]]])
+    
+    def test_query_bad_query(self):
+        try:
+            result_id_list('headword="a" and headword="b" or headword="c"', self.db)
+        except ValueError as e:
+            self.assertEqual(str(e), 'More than one type of logic operator without any brackets')
 
 if __name__ == '__main__':
     sys.stderr.write('SQLite version: ' + sqlite3.connect(':memory:').execute('SELECT sqlite_version();').fetchall()[0][0] + '\n')
