df0e631ded32b39130cc0dc1e0230a4bab887c6b
Marek Medveď <marek.medved@sketchengine.eu>
2024-03-14 18:24:54 +0100

    test_clean: fix loging info

--- website/tests/clean_nvh_test.py
+++ website/tests/clean_nvh_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
 # Author: Marek Medved, marek.medved@sketchengine.eu, Lexical Computing CZ
+import io
 import sys
 import unittest
 import fileinput
@@ -15,20 +16,22 @@ class TestQueries(unittest.TestCase):
         self.import_nvh = nvh.parse_file(fileinput.input('example_nvh/to_clean.nvh'))
 
     def test_rm_duplicates(self):
-        clean_log = []
-        self.import_nvh.clean_duplicate_nodes(log=clean_log)
-        self.assertListEqual(clean_log, ['removing duplicate node image: to_remove',
-                                         'removing duplicate node notes: to_remove'])
+        log = io.StringIO()
+        self.import_nvh.clean_duplicate_nodes(out=log)
+        log.seek(0)
+        self.assertEqual(log.read(), 'WARNING: removing duplicate node image: to_remove\n'
+                                     'WARNING: removing duplicate node notes: to_remove\n')
 
     def test_rename(self):
         rename_dict = {}
-        rename_log = []
-        self.import_nvh.rename_nodes(rename_dict, log=rename_log)
-        self.assertListEqual(rename_log, ['renaming node name to preserve uniqueness lemma -> sense_lemma',
-                                          'renaming node name to preserve uniqueness flag -> image_flag',
-                                          'renaming node name to preserve uniqueness quality -> example_quality',
-                                          'renaming node name to preserve uniqueness source -> lang_source',
-                                          'renaming node name to preserve uniqueness flag -> lang_flag'])
+        log = io.StringIO()
+        self.import_nvh.rename_nodes(rename_dict, out=log)
+        log.seek(0)
+        self.assertEqual(log.read(), 'WARNING: renaming node name to preserve uniqueness lemma -> sense_lemma\n' 
+                                     'WARNING: renaming node name to preserve uniqueness flag -> image_flag\n' 
+                                     'WARNING: renaming node name to preserve uniqueness quality -> example_quality\n' 
+                                     'WARNING: renaming node name to preserve uniqueness source -> lang_source\n' 
+                                     'WARNING: renaming node name to preserve uniqueness flag -> lang_flag\n')
 
 if __name__ == '__main__':
     unittest.main()
