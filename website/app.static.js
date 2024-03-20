64cccad235e15912aacc801c29acfc2972d0f60f
Tomáš Svoboda <tomas.svoboda@sketchengine.co.uk>
2024-03-20 10:17:30 +0100

    added non-minified materialize.js

--- website/app.static.js
+++ website/app.static.js
@@ -1,7 +1,7 @@
 import './libs/riot/riot.js'
 import './libs/riot/route.js'
 import './libs/riot/observable.js'
-import './libs/materialize/materialize.min.js'
+import './libs/materialize/materialize.js'
 import './libs/materialize/materialize-colorpicker.min.js'
 import './libs/fuzzysort/fuzzysort.js'
 import './libs/sortable/sortable.min.js'
