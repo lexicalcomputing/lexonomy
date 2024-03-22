6ba9843b44c29a55a2ef31277399315f2b2c4691
Tomáš Svoboda <tomas.svoboda@sketchengine.co.uk>
2024-03-22 12:24:10 +0100

    Removed old Lexonomy related code (entry/template/structure editor, widgets, styles, images)

--- website/furniture/screenful-user-config.js
+++
@@ -1,7 +0,0 @@
-Screenful.User.homeUrl=rootPath;
-Screenful.User.loginUrl=rootPath+"login/";
-Screenful.User.logoutUrl=rootPath+"logout/";
-Screenful.User.changePwdUrl=rootPath+"changepwd/";
-Screenful.User.forgotPwdUrl=rootPath+"forgotpwd/";
-Screenful.User.signupUrl=rootPath+"signup/";
-Screenful.User.profileUrl=rootPath+"userprofile/";
