857f6d9de7eb5c96813a8c9ee59951d5166756a9
Marek Medveď <marek.medved@sketchengine.eu>
2024-06-03 15:13:38 +0200

    projects new endpoints create/assign/reject/merge batches + tests

---
+++ website/gen_next_batch.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python3.10
+# coding: utf-8
+# usage: gen_next_batch.py BATCH_SIZE [MAX_BATCHES] [EXISTING_BATCHES_FILEMASK] [USER_EMAIL]
+import os
+import sys
+import glob
+import ops
+import json
+import subprocess
+from datetime import datetime
+
+currdir = os.path.dirname(os.path.abspath(__file__))
+siteconfig = json.load(open(os.path.join(currdir, "siteconfig.json"), encoding="utf-8"))
+
+def log_info(msg):
+    sys.stderr.write(f'INFO {datetime.now().strftime("[%Y-%m-%d, %H:%M:%S]")}: {msg}\n')
+
+
+def log_err(msg):
+    sys.stderr.write(f'ERROR {datetime.now().strftime("[%Y-%m-%d, %H:%M:%S]")}: {msg}\n')
+
+
+def log_warning(msg):
+    sys.stderr.write(f'WARNING {datetime.now().strftime("[%Y-%m-%d, %H:%M:%S]")}: {msg}\n')
+
+
+def get_already_exported(batch_list, tl_name):
+    already_exported = set()
+    for filename in batch_list:
+        for line in open(filename):
+            if line.startswith(tl_name + ': '):
+                already_exported.add(line.strip('\n').split(': ', 1)[1])
+    return already_exported
+
+
+def split_to_batches(input, max_batches, batch_size , batch_list, tl_node, already_exported, batch_dir, stage):
+    remaining_hws = 0
+    hw_idx = -1
+    batches_generated = 0
+    curr_batch = None
+    next_batch_num = len(batch_list) + 1
+    new_batches = []
+
+    for line in input:
+        if batches_generated > max_batches:
+            if curr_batch:
+                curr_batch.close()
+                curr_batch = None
+            if line.startswith(tl_node + ': '):
+                remaining_hws += 1
+        else:
+            # FILE MANAGMENT LOGIC
+            if line.startswith(tl_node + ': '):
+                hw = line.strip('\n').split(': ', 1)[1]
+                if hw not in already_exported:
+                    hw_idx += 1
+
+                # NEW BATCH FILE
+                if hw_idx % batch_size == 0: 
+                    if hw_idx > 0:
+                        curr_batch.close()
+
+                    batches_generated += 1
+                    if batches_generated > max_batches:
+                        remaining_hws += 1
+                        continue
+
+                    new_filename = batch_dir + '/' + stage + '.batch_%03d.in' % next_batch_num
+                    while os.path.exists(new_filename):
+                        next_batch_num += 1
+                        new_filename = batch_dir + '/' + stage + '.batch_%03d.in' % next_batch_num
+
+                    curr_batch = open(new_filename, 'w')
+                    next_batch_num += 1
+                    new_batches.append(new_filename)
+                    log_info("SPLITTING_BATCHES: PER:%.2d, COUNT:%d/%d, BATCH: %s" % ((batches_generated/ max_batches*100),
+                                                                                      batches_generated,  max_batches, new_filename))
+            # WRITING TO CURRENT BATCH
+            if hw not in already_exported:
+                curr_batch.write(line)
+
+    return new_batches, remaining_hws
+
+
+def update_project_info(batch_size, user, new_batches, project_id, stage, tl_node, remaining_hws, src_dict_id):
+    main_db = ops.getMainDB()
+    insert_project_dicts = []
+
+    for nvh_file_path in new_batches:
+        file_name = nvh_file_path.rsplit('/', 1)[1]
+
+        dict_id = ops.suggestDictId()
+        batch_name = file_name.rstrip('.in')
+
+        c = main_db.execute('SELECT * FROM projects WHERE id=?', (project_id,))
+        r = c.fetchone()
+        language = r['language']
+
+        dictDB = ops.initDict(dict_id, project_id + '.' + batch_name, language, "", user)
+        dict_config = {"limits": {"entries": int(batch_size)}}
+        ops.attachDict(dictDB, dict_id, {}, dict_config)
+
+        logfile_f = open(nvh_file_path + ".log", "w")
+        dbpath = os.path.join(siteconfig["dataDir"], "dicts/"+dict_id+".sqlite")
+        subprocess.Popen([currdir + "/import.py", dbpath, nvh_file_path, project_id, tl_node],
+                          stdout=logfile_f, stderr=logfile_f, start_new_session=True, close_fds=True)
+
+        log_info('IMPORTED: ' + nvh_file_path)
+
+        insert_project_dicts.append((project_id, dict_id, nvh_file_path, stage, json.dumps({'total': batch_size}), 'inProgress'))
+
+    main_db.executemany("INSERT INTO project_dicts (project_id, dict_id, source_nvh, stage, remaining, status) VALUES (?,?,?,?,?,?)", insert_project_dicts)
+    c3 = main_db.execute('SELECT remaining FROM project_dicts WHERE dict_id=?', (src_dict_id,))
+    r3 = c3.fetchone()
+    try:
+        is_remaining = json.loads(r3['remaining'])
+    except TypeError:
+        is_remaining = {}
+    is_remaining[stage] = remaining_hws
+    main_db.execute('UPDATE project_dicts SET remaining=? WHERE dict_id=?', (json.dumps(is_remaining), src_dict_id))
+    main_db.commit()
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Split input NVH into batches and import to project')
+    parser.add_argument('batch_size', type=int, help='Size of one batch')
+    parser.add_argument('max_batches', type=int, help='Number of batches')
+    parser.add_argument('generated_batches_filemask', type=str, help='Already annotated batches file mask')
+    parser.add_argument('user', type=str, help='User email')
+    parser.add_argument('src_dict_id', type=str, help='Source dict id')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'),
+                        required=False, default=sys.stdin,
+                        help='Input NVH')
+    parser.add_argument('--split_only', action='store_true',
+                        required=False, default=False,
+                        help='Only splits the input nvh into batches without project database update')
+    args = parser.parse_args()
+
+    # ==========================
+    # INIT
+    # ==========================
+    batch_list = glob.glob(args.generated_batches_filemask)
+    path_spl = args.generated_batches_filemask.split('/')
+    batch_dir = '/'.join(path_spl[:-1])
+    project_id = path_spl[-3]
+    stage = path_spl[-2]
+    tl_node = 'entry' # TODO load form db
+
+
+    if not os.path.exists(batch_dir):
+        os.makedirs(batch_dir)
+    # ==========================
+
+    # ==========================
+    # SPLIT TO BATCHES
+    # ==========================
+    already_exported = get_already_exported(batch_list, tl_node)
+    new_batches, remaining_hws = split_to_batches(args.input, args.max_batches, args.batch_size , batch_list, tl_node, already_exported, batch_dir, stage)
+    # ==========================
+
+    # ==========================
+    # INSERT TO DB
+    # ==========================
+    if not args.split_only:
+        update_project_info(args.batch_size, args.user, new_batches, project_id, stage, tl_node, remaining_hws, args.src_dict_id.rstrip('.nvh'))
+
+if __name__ == '__main__':
+    main()
--- website/tests/project_test.py
+++ website/tests/project_test.py
@@ -1,13 +1,21 @@
-#! /usr/bin/python3.10
+#!/usr/bin/python3.10
+# coding: utf-8
+# Author: Marek Medved, marek.medved@sketchengine.eu, Lexical Computing CZ
+import os
 import requests
 import json
 import unittest
 import config
+import time
+import random
+from pprint import pprint
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 class TestQueries(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.verbose = False
         cls.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         cls.website = config.website
 
@@ -25,34 +33,95 @@ class TestQueries(unittest.TestCase):
         cls.new_project_id = r2.json()['suggested']
 
         # get workflow name
-        r3 = requests.get(url=cls.website + "/wokflows/list.json",
-                          headers=cls.headers, cookies=cls.cookies)
-        cls.workflow = r3.json()['workflows'][0]['name']
+        # r3 = requests.get(url=cls.website + "/wokflows/list.json",
+        #                   headers=cls.headers, cookies=cls.cookies)
+        cls.workflow = 'test_workflow_new'
+
+        cls.source_dict_id = ''
+        cls.batch_log_file = ''
+        cls.all_batches_dict_ids = []
+
+        # TODO RM
+        cls.source_dict_id = 'marek_project_dict'
+        cls.new_project_id = 'marek_project'
+
+    @classmethod
+    def update_source_dict(cls, value):
+        cls.source_dict_id = value
+
+    @classmethod
+    def update_batch_log_file(cls, value):
+        cls.batch_log_file = value
+
+    @classmethod
+    def update_all_batches(cls):
+        API_ENDPOINT_0 = cls.website + f"/projects/{cls.new_project_id}/project.json"
+        r0 = requests.get(url=API_ENDPOINT_0, headers=cls.headers, cookies=cls.cookies)
+
+        for stage in r0.json()['workflow']:
+            for batch_dict in stage['batches']:
+                cls.all_batches_dict_ids.append((stage['stage'], batch_dict['dictID'], batch_dict['title']))
+
+    # SOURCE DICT CREATE
+    def test_1(self):
+        data = {'url': self.source_dict_id,
+                'hwNode': 'entry',
+                'title': self.source_dict_id,
+                'addExamples': 'false',
+                'deduplicate': 'false',
+                'language': 'en'
+                }
+        f = open(os.path.join(current_dir, 'example_nvh', 'project_source.nvh'), 'rb')
+        files = {'filename': f}
+
+        r = requests.post(url=self.website + "/make.json", data=data, files=files, cookies=self.cookies)
+        f.close()
+
+        if self.verbose:
+            print('================================')
+            print('CREATE DICT')
+            print('================================')
+            pprint(r.json())
+
+        self.assertEqual(r.json()['success'], True)
+        self.update_source_dict(r.json()['url'])
 
     # CREATE PROJECT
-    def test_1(self):
+    def test_2(self):
         API_ENDPOINT_1 = self.website + "/projects/create.json"
-        data = {'id': self.new_project_id ,
+        data = {'id': self.new_project_id,
                 'name': 'LCL test project',
                 'description': 'This is a testing project',
                 'annotators': json.dumps(['marek.medved3@gmail.com', 'marek.medved@sketchengine.eu', 'marek.medved@sketchengine.co.uk']),
                 'managers': json.dumps(['marek.medved3@gmail.com', 'marek.medved@sketchengine.eu', 'marek.medved@sketchengine.co.uk']),
-                'ref_corpus': 'test_ref_corpus',
-                'source_dict': 'test_source_corpus',
+                'ref_corpus': 'ententen21_tt31',
+                'source_dict_id': self.source_dict_id,
                 'workflow': self.workflow,
                 'language': 'cs'}
         r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
         self.assertEqual(r1.json()['success'], True)
         self.assertEqual(r1.json()['projectID'], self.new_project_id)
 
-        # test if created correctly
+        # ================
+        # PROJECT STATE
+        # ================
         API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/project.json"
         r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+
+        if self.verbose:
+            print('================================')
+            print('CREATE PROJECT')
+            print('================================')
+            pprint(r2.json())
+
         self.assertEqual(r2.json()['description'], 'This is a testing project')
         self.assertEqual(set(r2.json()['managers']) - set(["marek.medved3@gmail.com", "marek.medved@sketchengine.co.uk", "marek.medved@sketchengine.eu"]), set())
         self.assertEqual(set(r2.json()['annotators']) - set(["marek.medved3@gmail.com", "marek.medved@sketchengine.co.uk", "marek.medved@sketchengine.eu"]), set())
+        # ================
 
-        # test if created in user_projects
+        # ================
+        # IN PROJECT LISTING
+        # ================
         API_ENDPOINT_3 = self.website + "/projects/list.json"
         r3 = requests.get(url=API_ENDPOINT_3, headers=self.headers, cookies=self.cookies)
         found = False
@@ -60,40 +129,235 @@ class TestQueries(unittest.TestCase):
             if x['id'] == self.new_project_id:
                 found = True
         self.assertEqual(found, True)
+        # ================
 
     # EDIT PROJECT
-    def test_2(self):
+    def test_3(self):
         API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/update.json"
         data = {'name': 'LCL test project updated',
                 'description': 'This is a testing project updated',
-                'annotators': json.dumps(['marek.medved3@gmail.com', 'marek.medved@sketchengine.eu', 'marek.medved@sketchengine.co.uk', 'xmedved1.fi.muni.cz']),
+                'annotators': json.dumps(['marek.medved3@gmail.com', 'marek.medved@sketchengine.co.uk', 'xmedved1.fi.muni.cz']),
                 'managers': json.dumps(['marek.medved@sketchengine.eu']),
                 }
         r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
         self.assertEqual(r1.json()['success'], True)
 
-        # test if updated
+        # ================
+        # PROJECT STATE
+        # ================
         API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/project.json"
         r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+
+        if self.verbose:
+            print('================================')
+            print('EDIT PROJECT')
+            print('================================')
+            pprint(r2.json())
+
         self.assertEqual(r2.json()['description'], 'This is a testing project updated')
         self.assertEqual(set(r2.json()['managers']) - set(["marek.medved@sketchengine.eu"]), set())
-        self.assertEqual(set(r2.json()['annotators']) - set(['marek.medved3@gmail.com', 'marek.medved@sketchengine.co.uk', 'marek.medved@sketchengine.eu', 'xmedved1.fi.muni.cz']), set())
+        self.assertEqual(set(r2.json()['annotators']) - set(['marek.medved3@gmail.com', 'marek.medved@sketchengine.co.uk', 'xmedved1.fi.muni.cz']), set())
+        # ================
+
+    # CREATE BATCH
+    def test_4(self):
+        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/create_batch.json"
+        data = {'stage': 'sensitive',
+                'size': 1,
+                'batch_number': 3,
+                }
+        r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r1.json()['success'], True)
+
+        API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/create_batch.json"
+        data = {'stage': 'images',
+                'size': 1,
+                'batch_number': 3,
+                }
+        r2 = requests.post(url=API_ENDPOINT_2, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r2.json()['success'], True)
+
+        # TODO
+        # API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/getBatchesStatus.json"
+        # data = {'stage': 'sensitive'}
+        # r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+        # self.assertEqual(r2.json()['description'], 'This is a testing project updated')
+        # self.assertEqual(set(r2.json()['managers']) - set(["marek.medved@sketchengine.eu"]), set())
+        # self.assertEqual(set(r2.json()['annotators']) - set(['marek.medved3@gmail.com', 'marek.medved@sketchengine.co.uk', 'marek.medved@sketchengine.eu', 'xmedved1.fi.muni.cz']), set())
+
+        # ================
+        # PROJECT STATE
+        # ================
+        API_ENDPOINT_3 = self.website + f"/projects/{self.new_project_id}/project.json"
+        r3 = requests.get(url=API_ENDPOINT_3, headers=self.headers, cookies=self.cookies)
+
+        if self.verbose:
+            print('================================')
+            print('CREATE BATCH')
+            print('================================')
+            pprint(r3.json())
+
+        self.assertEqual(r3.json()['workflow'][0]['batches'][0]['nvh'].rsplit('/',1)[1], 'sensitive.batch_001.in')
+        self.assertEqual(r3.json()['workflow'][0]['batches'][0]['status'], 'inProgress')
+        self.assertEqual(r3.json()['workflow'][0]['batches'][0]['assignee'], None)
+        self.assertEqual(r3.json()['workflow'][0]['inputDicts'][0]['remaining'], 2)
+
+        self.assertEqual(r3.json()['workflow'][1]['batches'][0]['nvh'].rsplit('/',1)[1], 'images.batch_001.in')
+        self.assertEqual(r3.json()['workflow'][1]['batches'][0]['status'], 'inProgress')
+        self.assertEqual(r3.json()['workflow'][1]['batches'][0]['assignee'], None)
+        self.assertEqual(r3.json()['workflow'][1]['inputDicts'][0]['remaining'], 2)
+        # ================
+
+        # get dicIDs of all batches
+        self.update_all_batches()
+
+    # ASSIGN BATCH
+    def test_5(self):
+        # ================
+        # ASSIGN ALL BATCHES 
+        # ================
+        API_ENDPOINT_1 = self.website + f"projects/{self.new_project_id}/assign_batch.json"
+        data = {'assignees': json.dumps([(x[1], "marek.medved@sketchengine.eu") for x in self.all_batches_dict_ids])}
+        r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r1.json()['success'], True)
+
+        # ================
+        # ANNOTATE dicts
+        # ================
+        sensitive_values = ['yes', 'no']
+        images_values = ['my_image.jpg', 'this_image.svg', 'just_image.png']
+        for s, dict_id, title in self.all_batches_dict_ids:
+            API_ENDPOINT_2 = self.website + f"{dict_id}/entryread.json"
+            data = {'id': 1}
+            r2 = requests.post(url=API_ENDPOINT_2, data=data, headers=self.headers, cookies=self.cookies)
+
+            if s == 'sensitive':
+                annot_nvh = str(r2.json()['nvh']) + \
+                            f'    sensitive: {random.choice(sensitive_values)}\n' + \
+                            f'    __lexonomy_completed__: {title}\n'
+            elif s == 'images':
+                annot_nvh = str(r2.json()['nvh']) + \
+                            f'    image: {random.choice(images_values)}\n' + \
+                            f'    __lexonomy_completed__: {title}\n'
+            else:
+                annot_nvh = str(r2.json()['nvh']) + \
+                            f'   __lexonomy_completed__: {title}\n'
+
+            API_ENDPOINT_3 = self.website + f"{dict_id}/entryupdate.json"
+            data = {'id': 1,
+                    'nvh': annot_nvh}
+            r3 = requests.post(url=API_ENDPOINT_3, data=data, headers=self.headers, cookies=self.cookies)
+            self.assertEqual(r3.json()['success'], True)
+
+        # ================
+        # PROJECT STATE
+        # ================
+        API_ENDPOINT_4 = self.website + f"/projects/{self.new_project_id}/project.json"
+        r4 = requests.get(url=API_ENDPOINT_4, headers=self.headers, cookies=self.cookies)
+
+        if self.verbose:
+            print('================================')
+            print('ASSIG BATCH')
+            print('================================')
+            pprint(r4.json())
+
+        self.assertEqual(r4.json()['workflow'][0]['batches'][0]['assignee'], 'marek.medved@sketchengine.eu')
+        # ================
+
+    # REJECT BATCH
+    def test_6(self):
+        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/reject_batch.json"
+        data = {'dictID_list': json.dumps([self.all_batches_dict_ids[0][1]])}
+        r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r1.json()['success'], True)
+
+        # ================
+        # PROJECT STATE
+        # ================
+        API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/project.json"
+        r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+
+        if self.verbose:
+            print('================================')
+            print('REJECT BATCH')
+            print('================================')
+            pprint(r2.json())
+
+        for s in r2.json()['workflow']:
+            for b in s['batches']:
+                if b['dictID'] == self.all_batches_dict_ids[0][1]:
+                    self.assertEqual(b['status'], 'reject')
+                    self.assertTrue(b['nvh'].endswith('.rejected'))
+        # ================
+
+    # ACCEPT BATCH
+    def test_7(self):
+        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/accept_batch.json"
+        data = {'dictID_list': json.dumps([x[1] for x in self.all_batches_dict_ids])}
+        r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r1.json()['success'], True)
+
+        # ================
+        # PROJECT STATE
+        # ================
+        API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/project.json"
+        r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+        if self.verbose:
+            print('================================')
+            print('ACCEPT BATCH')
+            print('================================')
+            pprint(r2.json())
+
+        for s in r2.json()['workflow']:
+            for b in s['batches']:
+                self.assertEqual(b['status'], 'accept')
+        # ================
+
+    # MERGE SENSITIVE/IMAGES
+    def test_8(self):
+        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/make_stage.json"
+        data = {'stage': 'sensitive'}
+        r1 = requests.post(url=API_ENDPOINT_1, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r1.json()['success'], True)
+
+        API_ENDPOINT_2 = self.website + f"/projects/{self.new_project_id}/make_stage.json"
+        data = {'stage': 'images'}
+        r2 = requests.post(url=API_ENDPOINT_2, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r2.json()['success'], True)
+
+        API_ENDPOINT_3 = self.website + f"/projects/{self.new_project_id}/make_stage.json"
+        data = {'stage': 'final'}
+        r3 = requests.post(url=API_ENDPOINT_3, data=data, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r3.json()['success'], True)
+
+        # ================
+        # PROJECT STATE
+        # ================
+        API_ENDPOINT_4 = self.website + f"/projects/{self.new_project_id}/project.json"
+        r4 = requests.get(url=API_ENDPOINT_4, headers=self.headers, cookies=self.cookies)
+        if self.verbose:
+            print('================================')
+            print('MERGE SENSITIVE')
+            print('================================')
+            pprint(r4.json())
 
     # DELETE PROJECT
-    def test_3(self):
-        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/archive.json"
+    def test_9(self):
+        API_ENDPOINT_1 = self.website + f"/projects/{self.new_project_id}/delete.json"
         r1 = requests.post(url=API_ENDPOINT_1, headers=self.headers, cookies=self.cookies)
         self.assertEqual(r1.json()['success'], True)
         self.assertEqual(r1.json()['projectID'], self.new_project_id)
 
-        # test if deleted form list
+        # ================
+        # PROJECT DELETED FORM PROJECT LIST
+        # ================
         API_ENDPOINT_2 = self.website + "/projects/list.json"
-        r = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
-        found = False
-        for x in r.json()['projects_archived']:
-            if x['id'] == self.new_project_id:
-                found = True
-        self.assertEqual(found, True)
+        r2 = requests.get(url=API_ENDPOINT_2, headers=self.headers, cookies=self.cookies)
+        self.assertEqual(r2.json()['projects_active'], [])
+        self.assertEqual(r2.json()['projects_archived'], [])
+        self.assertEqual(r2.json()['total'], 0)
+        # ================
+
 
 if __name__ == '__main__':
     unittest.main()
