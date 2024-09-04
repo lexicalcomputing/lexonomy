59632b0a7b8b6d83ee918d3293ecf6805dae3498
Marek Medveď <marek.medved@sketchengine.eu>
2024-09-04 13:46:30 +0200

    export error info

--- website/adminscripts/export.py
+++ website/adminscripts/export.py
@@ -20,8 +20,16 @@ def main():
     args = parser.parse_args()
 
     dictDB = ops.getDB(args.dict_id)
-    for line in ops.download(dictDB, args.dict_id, args.format):
-        args.output.write(line)
+    try:
+        sys.stderr.write(f'INFO: processing {args.dict_id}\n')
+        for line in ops.download(dictDB, args.dict_id, args.format):
+            args.output.write(line)
+    except AttributeError as e:
+        if "execute" in str(e):
+            sys.stderr.write(f'ERROR: dictionary "{args.dict_id}" does not exists\n')
+        else:
+            sys.stderr.write(f'ERROR: {str(e)}\n')
+        sys.exit()
 
     dictDB.close()
 
