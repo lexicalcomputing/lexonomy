853a6487ba79c140fdb64e32e5f8d5e02d9b0965
Tomáš Svoboda <tomas.svoboda@sketchengine.co.uk>
2024-11-01 17:52:10 +0100

    - changed Custom entry editor page layout - added CodeMirror (JS/CSS... editor, dynamically loaded) - replaced CodeJar editor with better CodeMirror on configuration pages (Custom entry editor, Custom styles, Formatting) - removed prism.js

---
+++ website/codemirror.js
@@ -0,0 +1,11 @@
+import {EditorView, basicSetup} from "codemirror"
+import {javascript} from "@codemirror/lang-javascript"
+import {css} from "@codemirror/lang-css"
+
+window.CodeMirror = {
+  EditorView,
+  basicSetup,
+  javascript,
+  css
+}
+
