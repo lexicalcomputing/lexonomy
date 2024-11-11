8064b33868034504d2a940cef4cbde0ed8050002
Tomáš Svoboda <tomas.svoboda@sketchengine.eu>
2024-11-11 18:19:32 +0100

    riot requires and imports are now automatically generated

--- website/rollup.config.js
+++ website/rollup.config.js
@@ -2,12 +2,49 @@ import riot from 'rollup-plugin-riot'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import { registerPreprocessor } from '@riotjs/compiler'
 import sass from 'sass'
+import fs from  "fs"
+import path from "path"
 import css from "rollup-plugin-import-css"
 import { terser } from "rollup-plugin-terser"
 
 
+
+let walk = function(dir) {
+   // find all files in directory and its subdirectories
+   let results = []
+   let list = fs.readdirSync(dir)
+   list.forEach(file => {
+      file = path.join(dir, file)
+      let stat = fs.statSync(file)
+      if (stat && stat.isDirectory()) {
+            results = results.concat(walk(file))
+      } else {
+            results.push(file.split(path.sep).join("/"))
+      }
+   })
+   return results
+}
+
+function createAppJs(){
+   // create app.js file to import and register all .riot components
+   let lines = []
+
+   walk("./riot").forEach(f => {
+      let fileName = path.basename(f, ".riot")
+      let normalizedName = fileName.replaceAll("-", "_")
+      lines.push(`import ${normalizedName} from "./${f}"`)
+      lines.push(`riot_register("${fileName}", ${normalizedName})`)
+   })
+   let riotRegisterComponents = lines.sort().join("\n")
+
+   let appTemplate = fs.readFileSync("./app.js.template", {encoding:"utf8", flag:"r"})
+   let appFile = appTemplate.replace("@RIOT_REGISTER_COMPONENTS@", riotRegisterComponents)
+
+   fs.writeFileSync("app.js", appFile)
+}
+createAppJs()
+
 registerPreprocessor('css', 'scss', function(code, { options }) {
-  const { file } = options
   const {css} = sass.renderSync({
     data: code
   })
@@ -17,7 +54,6 @@ registerPreprocessor('css', 'scss', function(code, { options }) {
   }
 })
 
-const options = {}
 export default [{
   input: 'app.js',
   output: {
@@ -26,7 +62,12 @@ export default [{
     strict: false
   },
   plugins: [
-    riot(options),
+    {
+      buildStart() {
+        console.log('building Lexonomy...');
+      },
+    },
+    riot(),
     terser(),
     nodeResolve()
   ]
@@ -38,7 +79,7 @@ export default [{
     format: 'iife',
     strict: false
   },
-  plugins: [terser()]
+  plugins: [terser(), nodeResolve()]
 }, {
   input: "app.css.js",
   output: {
