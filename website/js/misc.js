1672bf26af8572bae315fcddef2ece21f201ee6e
Tomáš Svoboda <tomas.svoboda@sketchengine.eu>
2025-08-05 09:22:24 +0200

    formatting editor - improved auto scrolling to the selected element

--- website/js/misc.js
+++ website/js/misc.js
@@ -61,15 +61,28 @@ window.scrollToTheEndOfInput = (selector) => {
    }, 0)
 }
 
-window.scrollIntoViewIfNeeded = (element, options={behavior: 'smooth', block: 'start'}) => {
+window.scrollIntoViewVerticallyIfNeeded = (element, options={behavior: "smooth", block: "nearest", inline: "nearest"}) => {
    if(element){
       let rect = element.getBoundingClientRect()
       let inViewport = rect.top >= 0
-         && rect.left >= 0
-         && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-         && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      if (!inViewport) {
-         element.scrollIntoView(options)
+            && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      if(!inViewport) {
+         let ancestor = element.parentElement
+         while(ancestor) {
+            let style = getComputedStyle(ancestor)
+            let overflowY = style.overflowY
+            let hasScrollableContent = ancestor.scrollHeight > ancestor.clientHeight
+            let isScrollable = ["auto", "scroll"].includes(overflowY) && hasScrollableContent
+
+            if(isScrollable) {
+               ancestor.scrollTo({
+                  top: rect.top - ancestor.getBoundingClientRect().top + ancestor.scrollTop - 100,
+                  ...options
+               })
+               return
+            }
+            ancestor = ancestor.parentElement
+         }
       }
    }
 }
