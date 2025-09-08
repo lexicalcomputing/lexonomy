ee6241e8ebae6d5a3b7765da5eea855c7e50378b
Tomáš Svoboda <tomas.svoboda@sketchengine.eu>
2025-09-08 23:03:00 +0200

    Projects:  - add background color to the each dictionary in input and output card  - added description to stage  - disable import if dictionary is busy
    Other:
     - moved getColorByIndex() to misc.js
     - few minor style updates

--- website/js/nvhschema.js
+++ website/js/nvhschema.js
@@ -68,13 +68,13 @@ class NvhSchemaClass{
 
    getElementColor(element){
       let idx = this.getElementList().indexOf(element)
-      return this.getColorByIndex(idx)
+      return window.getColorByIndex(idx)
    }
 
    getPathColor(elementPath){
       let idx = this.getElementList()
             .findIndex(element => element.path == elementPath)
-      return this.getColorByIndex(idx)
+      return window.getColorByIndex(idx)
    }
 
    getAllElementAncestors(element){
@@ -270,42 +270,6 @@ class NvhSchemaClass{
             re: parsed.groups.re ? parsed.groups.re.slice(1) : ""  // remove ~
          }
       }
-   }
-
-   getColorByIndex(idx){
-      return [
-         "#3366cc",
-         "#dc3912",
-         "#ff9900",
-         "#109618",
-         "#990099",
-         "#0099c6",
-         "#dd4477",
-         "#66aa00",
-         "#b82e2e",
-         "#316395",
-         "#994499",
-         "#22aa99",
-         "#aaaa11",
-         "#6633cc",
-         "#e67300",
-         "#8b0707",
-         "#651067",
-         "#329262",
-         "#5574a6",
-         "#3b3eac",
-         "#b77322",
-         "#16d620",
-         "#b91383",
-         "#f4359e",
-         "#9c5935",
-         "#a9c413",
-         "#2a778d",
-         "#668d1c",
-         "#bea413",
-         "#0c5922",
-         "#743411"
-      ][idx % 30] || "#000"
    }
 
    _validate(){
